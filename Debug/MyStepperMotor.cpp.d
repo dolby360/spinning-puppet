bf428b062c5ab983432bbc0d488d71057702903f
dolby360 <mrdolbyking@gmail.com>
2017-10-02 20:57:03 +0300

    Debug folder

---
+++ Debug/MyStepperMotor.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\dolev\AppData\Local\Temp\VMBuilds\spinning_puppet\uno\Debug\MyStepperMotor.cpp.o: \
+ C:\Users\dolev\AppData\Local\Temp\VMBuilds\spinning_puppet\uno\Debug\MyStepperMotor.cpp
---
+++ Debug/spinning_puppet.cpp.d
@@ -0,0 +1,17 @@
+C:\Users\dolev\AppData\Local\Temp\VMBuilds\spinning_puppet\uno\Debug\spinning_puppet.cpp.o: \
+ C:\Users\dolev\AppData\Local\Temp\VMBuilds\spinning_puppet\uno\Debug\spinning_puppet.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\dolev\AppData\Local\Temp\VMBuilds\spinning_puppet\uno\Debug\puppetSpinnerManager.h \
+ C:\Users\dolev\AppData\Local\Temp\VMBuilds\spinning_puppet\uno\Debug\SM.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Stepper\src/Stepper.h \
+ C:\Users\dolev\AppData\Local\Temp\VMBuilds\spinning_puppet\uno\Debug\Led.h
